203a9b3502b2f0afcd5afbd99c13796aa4409df4
Ao Tanuki <danfeedev@gmail.com>
2018-08-22 20:37:31 -0500

    First prototype of the frame

--- src/main/java/interfaz/Frame.java
+++ src/main/java/interfaz/Frame.java
@@ -1,18 +1,28 @@
 package interfaz;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Frame extends Application {
 
-	public void main (String[] args)
+	@Override
+	public void start(Stage primaryStage) throws Exception 
+	{
+		Parent root = FXMLLoader.load(getClass().getResource("/view/inicio.fxml"));
+		
+		Scene scene = new Scene(root);
+		
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+	}
+	
+	public static void main (String[] args)
 	{
 		launch(args);
-	}
-	@Override
-	public void start(Stage arg0) throws Exception {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
